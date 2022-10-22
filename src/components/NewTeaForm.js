d34854e9e3fb43ce8702754de382a0d3819c6d29
RosarioRu <mariarruval@gmail.com>
2022-10-21 22:57:28 -0700

    Use a table to format the form for new tea.

--- src/components/NewTeaForm.js
+++ src/components/NewTeaForm.js
@@ -3,9 +3,55 @@ import React from "react";
 function NewTeaForm(props) {
   return (
     <React.Fragment>
-      {/* <h3>Add a New Crate of Tea</h3><br/>
+      <br/>
+      <table class="table">
+        <thead>
+          <h3>Add a New Crate of Tea</h3>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <input type="text" name="name" placeholder="Tea name" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label for="type">Select type</label>
+              <select id="type" name="type">
+                <option value="green">Green</option>
+                <option value="black">Black</option>
+                <option value="oolong">Oolong</option>
+                <option value="herbal">Herbal</option>
+                <option value="blend">Blend</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input type="text" name="origin" placeholder="Origin" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input type="number" name="price" placeholder="Price per ounce" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input type="text" name="caffeineLevel" placeholder="Caffeine level" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+            <button type="submit">Add Crate</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+          
 
-      <input type="text" name="name" placeholder="Tea name" /><br/><br/>
+          
+      {/* <input type="text" name="name" placeholder="Tea name" /><br/><br/>
 
       <label for="type">Select type</label>
       <select id="type" name="type">
@@ -22,9 +68,8 @@ function NewTeaForm(props) {
 
       <input type="text" name="caffeineLevel" placeholder="Caffeine level" /> 
 
-      <button type="submit">Add Crate</button>
+      <button type="submit">Add Crate</button> */}
       
-      */}
     </React.Fragment>
   );
 }
