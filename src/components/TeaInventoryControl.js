3fb988ebcf6c315f727c2c929945c325fd0e780a
RosarioRu <mariarruval@gmail.com>
2022-10-21 17:09:59 -0700

    Add initial code for TeaInventoryControl class but COMMENT IT OUT as initially site will be static.

---
+++ src/components/TeaInventoryControl.js
@@ -0,0 +1,22 @@
+// import React from "react";
+
+// class TeaInventoryControl extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       //properties of state go here!
+//     };
+//   }
+
+//   render() {
+    
+//     return (
+//       <React.Fragment>
+
+//       </React.Fragment>
+
+//     );
+//   }
+
+// }
