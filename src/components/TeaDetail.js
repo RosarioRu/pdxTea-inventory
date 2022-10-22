fa9b4cbd9e0b61fdd08bf33960243e2441d20f08
RosarioRu <mariarruval@gmail.com>
2022-10-22 10:20:09 -0700

    Add initial TeaDetail component.

---
+++ src/components/TeaDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TeaDetail(props) {
+  return (
+    <React.Fragment>
+      <h1>Tea details will go here</h1>
+    </React.Fragment>
+  );
+}
+
+export default TeaDetail;
+  
