4c5d9b85d114703267b519f4d2e468ed09eee1a2
RosarioRu <mariarruval@gmail.com>
2022-10-21 17:46:02 -0700

    Import and return the TeaList in the App() function.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./Header";
+import TeaList from "./TeaList";
 // import TeaInventoryControl from "./TeaInventoryControl";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <TeaList /> 
+      {/* <TeaInventoryControl /> */}
     </React.Fragment>
   );
 }
