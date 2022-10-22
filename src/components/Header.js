dc54cff47905011bddbfa832709427f9a5ff757e
RosarioRu <mariarruval@gmail.com>
2022-10-21 17:09:13 -0700

    Add and export Header component.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>Forest Park Tea</h1>
+      <p>Tranquil inventory tracking for the best teahouse in PDX</p>
+    </React.Fragment>
+  );
+}
+
+export default Header;
