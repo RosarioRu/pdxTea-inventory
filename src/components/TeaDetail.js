965650cbcd3f75da57c4c4591a768f210eb17a74
RosarioRu <mariarruval@gmail.com>
2022-10-22 11:06:37 -0700

    Update TeaDetail to display all the different properties of each tea.

--- src/components/TeaDetail.js
+++ src/components/TeaDetail.js
@@ -1,12 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function TeaDetail(props) {
   return (
     <React.Fragment>
-      <h1>Tea details will go here</h1>
+      <h4>{props.tea.name} Details</h4>
+      <h6>Type: {props.tea.type}</h6>
+      <h6>Origin: {props.tea.origin}</h6>
+      <h6>Price: ${props.tea.price}/oz</h6>
+      <h6>Caffeine: {props.tea.caffeineLevel}</h6>
+      <h6>Crates: {props.tea.numberOfCrates}</h6>
+      <h6>Ounce: {props.tea.amountInOunces}</h6>
     </React.Fragment>
   );
 }
 
+TeaDetail.propTypes = {
+  tea: PropTypes.object
+};
+  
 export default TeaDetail;
   
