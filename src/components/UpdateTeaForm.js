6668ade74470b2893487a81e9d9df29f7b6d92a6
RosarioRu <mariarruval@gmail.com>
2022-10-22 15:00:18 -0700

    Import PropTypes and add onUpdateFormSubmission as an incoming propType. Import v4 for adding a unique id.

--- src/components/UpdateTeaForm.js
+++ src/components/UpdateTeaForm.js
@@ -1,12 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { v4 } from "uuid"; 
+
 
 function UpdateTeaForm(props) {
   const updateFormStyles = {
     width: "50%",
     marginLeft: "auto",
     marginRight: "auto",
-  
     input: {
       textAlign: "center",
       marginLeft: "3px",
@@ -16,7 +17,7 @@ function UpdateTeaForm(props) {
   return (
     <React.Fragment>
 
-      <form style={updateFormStyles}>
+      <form style={updateFormStyles} onSubmit={handleUpdateTeaFormSubmission}>
         <br/>
         <h4>{props.tea.name}: Update Information</h4>
         <table className="table">
@@ -24,7 +25,20 @@ function UpdateTeaForm(props) {
             <tr>
               <td>
                 <label htmlFor="name">Name</label>
-                <input type="text" name="name" placeholder={props.tea.name} style={updateFormStyles.input} />
+                <input type="text" name="name" placeholder={props.tea.name} style={updateFormStyles.input} required />
+              </td>
+            </tr>
+            <tr> 
+              <td>
+                <label htmlFor="type">Select type</label>
+                <select id="type" name="type" required style={updateFormStyles.input}>
+                  <option></option>
+                  <option value="green">Green</option>
+                  <option value="black">Black</option>
+                  <option value="oolong">Oolong</option>
+                  <option value="herbal">Herbal</option>
+                  <option value="blend">Blend</option>
+                </select>
               </td>
             </tr>
             <tr>
@@ -36,18 +50,29 @@ function UpdateTeaForm(props) {
             <tr>
               <td>
                 <label htmlFor="price">Price</label>
-                <input type="number" step="0.01" min="1" name="price" placeholder={props.tea.price} style={updateFormStyles.input} />
+                <input type="number" step="0.01" min="1" name="price" placeholder={props.tea.price} style={updateFormStyles.input} required />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="caffeineLevel">Caffeine Level</label>
+                <select id="caffeineLevel" name="caffeineLevel" style={updateFormStyles.input}>
+                  <option value=""></option>
+                  <option value="none">none</option>
+                  <option value="low">low</option>
+                  <option value="medium">medium</option>
+                  <option value="high">high</option>
+                </select>
               </td>
             </tr>
             <tr>
               <td>
                 <label htmlFor="numberOfCrates">Crates</label> 
-                <input type="number" name="numberOfCrates" min="1" placeholder={props.tea.numberOfCrates} style={updateFormStyles.input}  />
+                <input type="number" name="numberOfCrates" min="1" placeholder={props.tea.numberOfCrates} style={updateFormStyles.input} required />
               </td>
             </tr>
             <tr>
               <td>
-              {/* button bellow should trigger function called handleUpdatingTeaFormSubmission() below */}
               <button type="submit" className="btn btn-secondary btn-sm">Update</button>
               </td>
             </tr>
@@ -56,14 +81,25 @@ function UpdateTeaForm(props) {
       </form> 
     </React.Fragment>
   );
+
+  function handleUpdateTeaFormSubmission(event) {
+    event.preventDefault();
+    props.onUpdateFormSubmission({
+      name: event.target.name.value,
+      type: event.target.type.value,
+      origin: event.target.origin.value,
+      price: parseFloat(event.target.price.value),
+      caffeineLevel: event.target.caffeineLevel.value,
+      numberOfCrates: parseInt(event.target.numberOfCrates.value),
+      amountInOunces: parseInt(event.target.numberOfCrates.value)*130,
+      id: v4(),
+    });
+  }
 }
-
-function handleUpdatingTeaFormSubmission() 
-//capture values from the edit form and then triggers the handleUpdatingTeaInList method in the TicketControl component.
-
 
 UpdateTeaForm.propTypes = {
   tea: PropTypes.object,
+  onUpdateFormSubmission: PropTypes.func,
 };
 
 export default UpdateTeaForm;
