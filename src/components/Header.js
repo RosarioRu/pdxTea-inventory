52081aa0bbe2be7bde64d87753f0f73d2b5ddcdf
RosarioRu <mariarruval@gmail.com>
2022-10-21 17:28:22 -0700

    Add styling object to Header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
 import React from "react";
 
 function Header() {
+
+  const headerStyles = {
+    border: "2px solid green",
+    paddingLeft: "2%",
+    paddingRight: "2%",
+    fontFamily: "'apple chancery', 'cursive'",
+    textAlign: "center",
+    h1: {
+      //empty for now
+    },
+    p: {
+      fontStyle: "italic",
+    }
+  }
+
   return (
     <React.Fragment>
-      <h1>Forest Park Tea</h1>
-      <p>Tranquil inventory tracking for the highest quality teas in PDX</p>
+      <div style={headerStyles}>
+        <br/><h1>Forest Park Tea</h1>
+        <p style={headerStyles.p}>Tranquil inventory tracking for the highest quality teas in PDX</p>
+      </div>
     </React.Fragment>
   );
 }
