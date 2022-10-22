946f9dbf760e59e306a352e56d9f386d138acc60
RosarioRu <mariarruval@gmail.com>
2022-10-21 21:43:29 -0700

    Add code for form but comment it out for now.

---
+++ src/components/NewTeaForm.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function NewTeaForm(props) {
+  return (
+    <React.Fragment>
+      {/* <h3>Add a New Crate of Tea</h3><br/>
+
+      <input type="text" name="name" placeholder="Tea name" /><br/><br/>
+
+      <label for="type">Select type</label>
+      <select id="type" name="type">
+        <option value="green">Green</option>
+        <option value="black">Black</option>
+        <option value="oolong">Oolong</option>
+        <option value="herbal">Herbal</option>
+        <option value="blend">Blend</option>
+      </select><br/><br/>
+
+      <input type="text" name="origin" placeholder="Origin" /><br/><br/>
+
+      <input type="number" name="price" placeholder="Price per ounce" /><br/><br/>
+
+      <input type="text" name="caffeineLevel" placeholder="Caffeine level" /> */}
+    </React.Fragment>
+  );
+}
+
+export default NewTeaForm;
