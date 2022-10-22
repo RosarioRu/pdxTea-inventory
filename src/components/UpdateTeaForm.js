511e774d169073af8ec58a9e2e1271d11cbb5a37
RosarioRu <mariarruval@gmail.com>
2022-10-22 12:22:23 -0700

    Add placeholder text where update form will live.

---
+++ src/components/UpdateTeaForm.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function UpdateTeaForm() {
+  return (
+    <React.Fragment>
+      <h1>Update form goes here</h1>
+    </React.Fragment>
+  );
+}
+
+UpdateTeaForm.propTypes = {
+  
+};
+
+export default UpdateTeaForm;
