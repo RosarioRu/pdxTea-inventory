750ee0146e29f91cb2282758c30ced8da0f63a5a
RosarioRu <mariarruval@gmail.com>
2022-10-22 08:47:55 -0700

    Update properties to match form input names and display name, type and ounces of tea.

--- src/components/Tea.js
+++ src/components/Tea.js
@@ -5,17 +5,16 @@ function Tea(props) {
   return (
     <React.Fragment>
       <h4>{props.name}</h4>
-      <h6>Amount of caffeine: {props.caffeine}</h6>
+      <h6>Type: {props.type}</h6>
       <h6>Available Ounces: {props.amountInOunces}</h6>
-      
     </React.Fragment>
   );
 }
 
 Tea.propTypes = {
-  name: PropTypes.string.isRequired,
-  caffeine: PropTypes.string,
-  amountInOunces: PropTypes.number.isRequired
+  name: PropTypes.string,
+  caffeineLevel: PropTypes.string,
+  amountInOunces: PropTypes.number,
 }
 
 export default Tea;
